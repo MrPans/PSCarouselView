985600da29fbe68f7bb3cce60da25e21bd69add9
Sheng Pan <developerpans@163.com>
2016-01-31 15:59:12 +0800

    update Demo in ScrollView

---
+++ CarouselDemo/CarouselDemo/CarouselInTableView/DemoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoTableViewController.h
+//  CarouselDemo
+//
+//  Created by Pan on 16/1/21.
+//  Copyright © 2016年 Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoTableViewController : UITableViewController
+
+@end
